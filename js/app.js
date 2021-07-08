6acbd81965fbf1a97a5273866b0567f332b9bf15
Chris <cleungdev@gmail.com>
2021-07-08 00:22:45 -0400

    updated js to open and close menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 // Hamburger menu
-
 // create a namespace object to hold the app
 const menu = {}
 
@@ -9,38 +8,55 @@ menu.init = () => {
   menu.btn = document.querySelector('.header__hamburger')
   menu.menuItems = document.querySelectorAll('.header__menu')
   menu.header = document.querySelector('.header')
+  menu.overlay = document.querySelector('.overlay')
+  menu.hiddenEls = document.querySelectorAll('.hidden')
+  menu.body = document.querySelector('body')
 
   menu.toggleMenu()
 }
 
 // create a function to open the menu
 menu.openMenu = () => {
-  // add an open class to the element
+  // add an open class to the header element
   menu.header.classList.add('open')
+  // remove hidden class from all elements
+  menu.hiddenEls.forEach(el => {
+    el.classList.remove('hidden')
+  })
+  // prevent scrolling when menu is opened
+  menu.body.classList.add('no-scroll')
 }
 // create a function to close and reset the menu
 menu.closeMenu = () => {
-  // remove the open class
+  console.log('close');
+  // reset classes to open state
   if(menu.header.classList.contains('open')) {
     menu.header.classList.remove('open')
   }
+  menu.hiddenEls.forEach(el => {
+    el.classList.add('hidden')
+  })
+  menu.body.classList.remove('no-scroll')
 }
 
 // create a function to toggle the menu
-// if the menu
 menu.toggleMenu = () => {
+  // add event listener to the button
   menu.btn.addEventListener('click', () => {
-
+  // if header contains open class close the menu
     if (menu.header.classList.contains('open')) {
       menu.closeMenu()
+      console.log('close');
+  // otherwise, open the menu
     } else {
       menu.openMenu()
+      console.log('open');
     }
 
     // close menu when a link is clicked
     menu.menuItems.forEach(item => {
       item.addEventListener('click', () => {
-        closeMenu()
+        menu.closeMenu()
       })
     })
   })
